e002c51ec6c65dd231abd732bd2b83cbce9e7aad
yuudaiSuzuki0825 <syforworking@gmail.com>
2023-02-20 20:09:37 +0900

    commit2

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -12,7 +12,6 @@ class TaskController extends Controller
     public function index()
     {
         $tasks = Task::all();
-        // $count = Count::count();
         return response()->json($tasks, 200);
     }
 
@@ -22,20 +21,14 @@ class TaskController extends Controller
         $task->title = $request->title;
         $task->content = $request->content;
         $task->save();
-        // $count = Count::count();
         return response()->json($tasks, 200);
     }
 
     public function delete(Request $request)
     {
         $task = Task::find($request->id);
-        // $count = new Count;
-        // $count->title = $task->title;
-        // $count->content = $task->content;
-        // $count->save();
         $task->delete();
         $tasks = Task::all();
-        // $count = Count::count();
         return response()->json($tasks, 200);
     }
 }
